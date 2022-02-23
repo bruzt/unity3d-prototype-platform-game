36d7dc5c4cc40c69d758a3b42cf6c4055c6daa03
Bruno Z <bzutim@gmail.com>
2022-02-23 16:24:09 -0300

    feat: rope implementation

---
+++ Assets/Scripts/RopeController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour
+{
+    private Transform node;
+
+    public int size = 1;
+    public float offsetNode = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        node = transform.GetChild(0);
+
+        for(int i=1; i < size; i++){
+            Vector3 position = node.transform.position;
+            position.y -= offsetNode;
+
+            GameObject newNode = Instantiate(node.gameObject, position, transform.rotation);
+            
+            newNode.transform.SetParent(transform);
+
+            newNode.GetComponent<FixedJoint>().connectedBody = node.GetComponent<Rigidbody>();
+
+            //Physics.IgnoreCollision(node.GetComponent<Collider>(), newNode.GetComponent<Collider>());
+            
+            newNode.GetComponent<Rigidbody>().mass *= 1.01f; 
+            newNode.GetComponent<RopeSegmentController>().IncreaseMaxXVelocity(size, i);
+            newNode.GetComponent<RopeSegmentController>().DecreaseMaxZRotationDegree(size, i);
+
+            node = newNode.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RopeSegmentController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSegmentController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    public float finalMaxXVelocity = 50;
+    public float maxXVelocity = 1;
+    public float finalMaxZRotationDegree = 80;
+    public float maxZRotationDegree = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        LimitXMaxVelocity();
+        LimitZRotationDegree();
+        LimitXPosition();
+    }
+
+    void LimitXMaxVelocity(){
+        Vector3 velocity = rigidBody.velocity;
+        velocity.x = Mathf.Clamp(velocity.x, -maxXVelocity, maxXVelocity);
+        rigidBody.velocity = velocity;
+    }
+
+    void LimitZRotationDegree(){
+        Quaternion rotation = transform.rotation;
+        rotation.z = Mathf.Clamp(rotation.z, -maxZRotationDegree, maxZRotationDegree);
+        transform.rotation = rotation;
+    }
+
+
+    void LimitXPosition(){
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, position.x-0.1f, position.x+0.1f);
+        transform.position = position;
+    }
+
+    public void IncreaseMaxXVelocity(int size, int position){
+
+        float percent = (float)position/(float)size;
+
+        float velocity = finalMaxXVelocity*percent;
+
+        maxXVelocity = velocity;
+    }
+
+    public void DecreaseMaxZRotationDegree(int size, int position){
+        
+        float percent = (float)position/(float)size;
+
+        float degree = finalMaxZRotationDegree*percent;
+
+        maxZRotationDegree = degree;
+    }
+}
